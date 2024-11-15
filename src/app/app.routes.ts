b3482dfb6ff4c96a569f0901fe65c03a799cb728
Gilgamesh2408 <hewmiguehernandez@gmail.com>
2024-11-15 17:30:31 -0500

    feat: Creating the admin page, working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ExcusesComponent } from './pages/excuses/excuses.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { LayoutComponent } from './shared/layout/layout.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { AdminComponent } from './admin/admin.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -11,6 +12,7 @@ export const routes: Routes = [
   { path: 'menu', component: MenuComponent },
   { path: 'excuse', component: ExcusesComponent },
   {path: 'layout', component:LayoutComponent},
-  {path: 'footer', component:FooterComponent}
+  {path: 'footer', component:FooterComponent},
+  {path: 'admin',component:AdminComponent}
 ];
 
